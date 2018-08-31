5c817e5398ee84604ce95974f048319dbf2f97ec
rweed <rweed@rob-moac>
2018-08-30 23:34:19 -0700

    add tedious

--- routes/user.js
+++ routes/user.js
@@ -6,10 +6,13 @@ const router = express.Router();
 /* GET user profile. */
 router.get('/', ensureLoggedIn, function(req, res, next) {
   res.render('user', {
+    pic: 'http://localhost/images/favicon.ico',
     user: req.user ,
     userProfile: JSON.stringify(req.user, null, '  ')
   });
-  console.log(`Hi ${req.user.user_id}`);
+  let email = req.user.displayName;
+  // console.log(`Hi ${req.user.user_id}`);
+  console.log(`Hi ${req.user.displayName}`);
 });
 
 module.exports = router;
