f453286dc7a727c214245e9e45d92a01ff41cffc
rweed <rweed@rob-moac>
2018-09-15 18:58:09 -0700

    clean up

--- routes/user.js
+++ routes/user.js
@@ -2,13 +2,9 @@ const express = require('express');
 const passport = require('passport');
 const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn();
 const router = express.Router();
-const myMongo = require('../mongo.js');
-
-const myDB = myMongo;
 
 /* GET user profile. */
 router.get('/', ensureLoggedIn, function(req, res, next) {
-  //const myUser = myDB.getUserById(req.user.user_id);
   res.render('user', {
     pic: 'http://localhost/images/favicon.ico',
     user: req.user ,
