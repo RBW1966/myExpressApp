b90fca7d7793deb22d2565ad15e9069e37e0ac47
Rob <rob_weed@hotmail.com>
2018-09-16 20:55:32 -0700

    test

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -4,10 +4,27 @@ window.addEventListener('load', () => {
 
 function doIt() {
   const socket = io();
+  socket.emit('register user', getCookie('user_id'));
   document.getElementById("form1").addEventListener('submit', function(evt) {
     const m = document.getElementById("m");
     evt.preventDefault();
     socket.emit('chat message', m.value);
     m.value = '';
   });
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+          c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+          return c.substring(name.length, c.length);
+      }
+  }
+  return "";
 }
